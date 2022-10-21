957e168b9a00cdab0f71c915167f177603fc3b80
GSoni <gn_soni@hotmail.com>
2022-10-21 12:57:57 +0100

    Initial version console application

---
+++ DBSQL.sql
@@ -0,0 +1,33 @@
+﻿Create Table Title
+( ID int not null primary key,
+  Description varchar(10) not null)
+
+
+Create table People
+( ID int not null primary key,
+  TitleID int not null,
+  FName varchar(15) not null, 
+  LName varchar(15) not null)
+
+Alter table People add constraint fk_people_title foreign key (TitleID) references Title(ID)
+
+
+
+Insert into Title values (1, 'Mr');
+Insert into Title values (2, 'Mrs');
+
+Insert into People Values(1, 1, 'Person 1','Surname 1')
+Insert into People Values(2, 2, 'Person 2','Surname 2')
+Go
+
+Create procedure GetPeople
+As
+Begin
+Select p.ID, 
+	   t.Description, 
+	   p.FName, 
+	   p.LName 
+  from People p 
+		inner join Title t on t.ID = p.TitleID
+end
+Go
---
+++ DemoConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Connection string
+ * Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+ */
+
+// See https://aka.ms/new-console-template for more information
+using System.Configuration;
+using System.Data.SqlClient;
+
+Console.WriteLine("List of People from DB");
+
+string connstr = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+using (SqlConnection con = new SqlConnection(connstr))
+{
+    con.Open();
+    string sql = "GetPeople";
+
+    using (SqlCommand cmd = new SqlCommand(sql, con))
+    {
+        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+        var reader = cmd.ExecuteReader();
+        while(reader.Read())
+        {
+            Console.WriteLine($"ID: {reader[0].ToString()} Title: {reader[1].ToString()} First Name {reader[2].ToString()} Last Name: {reader[3].ToString()}");
+        }
+    }
+}
